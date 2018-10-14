266e6bf96e1667f730099fbd4d781709abc1396c
jeffo <jeffo@rulez.com>
2018-10-14 14:17:38 -0400

    setup.py tweaks

--- setup.py
+++ setup.py
@@ -4,4 +4,13 @@ from distutils.core import setup
 setup(
     name='aws-profile-gpg',
     version='0.0.1a',
+    description='call aws-cli using access keys from an encrypted credentials file',
+    long_description=open('README.md').read(),
+    url='https://github.com/jefforulez/aws-profile-gpg',
+    python_requires='~=2.7',
+    entry_points={
+        'console_scripts': [
+            'aws-profile-gpg=aws-profile-gpg:main',
+        ],
+    },
 )
